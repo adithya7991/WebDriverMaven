8a8d5f1d8f08d9e4e52f15e13abbddabc8efd74c
Adithya A <adithyaanand1151997@gmail.com>
2020-04-06 21:35:46 -0700

    Initial commit

---
+++ src/com/testng/NewTest.java
@@ -0,0 +1,14 @@
+package com.testng;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("hello");
+  }
+  
+
+}
